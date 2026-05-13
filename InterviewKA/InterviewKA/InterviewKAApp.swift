ed9fc70a2f9026efe34216e047470be6995da462
George Iordache <georgeiordache2000@yahoo.com>
2026-05-13 14:26:26 +0300

    created project

---
+++ InterviewKA/InterviewKA/InterviewKAApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewKAApp.swift
+//  InterviewKA
+//
+//  Created by George on 13.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct InterviewKAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
